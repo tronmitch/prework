d88a781639c19972e9c53657a3703c4ca81ac1d3
tronmitch <theron.mitchell@gmail.com>
2023-08-19 18:07:35 -0600

    added jave script

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// var topic = "HTML"
+// if (topic === "HTML") {
+//     console.log("Let's study HTML!");
+//    } else if (topic === 'CSS') {
+//     console.log("Let's study CSS!");
+//    } else if (topic === 'Git') {
+//     console.log("Let's study Git!");
+//    } else if (topic === 'JavaScript') {
+//     console.log("Let's study JavaScript!");
+//    } else {
+//     console.log('Please try again!');
+//    }
+
+// var shapes = ["triangle","square","pentagon","circle"]
+// var studentInfo =["lu",54,true];
+// console.log(shapes[0]);
+// for(var x = 0; x<shapes.length;x++){
+//     console.log(shapes[x]);
+// }
+var topics = ["HTML","CSS","GIT","JS"]
+for (x=0; x<topics.length;x++){
+    console.log(topics[x])
+}
