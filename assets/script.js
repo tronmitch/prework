0aa1b0176b39e36886dfa3a5eafdf78a8bd73202
tronmitch <theron.mitchell@gmail.com>
2023-08-19 19:34:08 -0600

    added JS code

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,29 @@
-// var topic = "HTML"
-// if (topic === "HTML") {
-//     console.log("Let's study HTML!");
-//    } else if (topic === 'CSS') {
-//     console.log("Let's study CSS!");
-//    } else if (topic === 'Git') {
-//     console.log("Let's study Git!");
-//    } else if (topic === 'JavaScript') {
-//     console.log("Let's study JavaScript!");
-//    } else {
-//     console.log('Please try again!');
-//    }
+
+var topics = ["HTML","CSS","GIT","JS"];
+var randomTopic = topics[Math.floor(Math.random()*topics.length)];
+
+
+function listTopics() {
+    for (x=0; x<topics.length;x++){
+        console.log(topics[x]);
+    }   
+}
+
+function selectTopics(){
+    if (randomTopic === "HTML") {
+        console.log("Let's study HTML!");
+       } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+       } else if (randomTopic === 'GIT') {
+        console.log("Let's study Git!");
+       } else if (randomTopic === 'JS') {
+        console.log("Let's study JavaScript!");
+       } else {
+        console.log('Please try again!');
+       }
+}
+
+
 
 // var shapes = ["triangle","square","pentagon","circle"]
 // var studentInfo =["lu",54,true];
@@ -17,7 +31,12 @@
 // for(var x = 0; x<shapes.length;x++){
 //     console.log(shapes[x]);
 // }
-var topics = ["HTML","CSS","GIT","JS"]
-for (x=0; x<topics.length;x++){
-    console.log(topics[x])
-}
+
+selectTopics();
+// listTopics();
+
+
+// function hellWorld(){
+//    console.log("Hello, World, I'm a function")
+// }
+// hellWorld();
